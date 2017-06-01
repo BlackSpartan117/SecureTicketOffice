d682ddf4948eb88833c7e1da82559a0cde16c376
dagui7 <jdagui7@live.com.mx>
2017-06-01 00:47:59 -0500

    cifradoAES pruebas

--- TicketESCOM/src/java/xml/xmlcuenta.java
+++ TicketESCOM/src/java/xml/xmlcuenta.java
@@ -23,8 +23,7 @@ public class xmlcuenta {
         Element cuenta = new Element("cuenta");
         Document doc = new Document(cuenta);
 
-        Element datos_cuenta = new Element("cuenta");
-        datos_cuenta.setAttribute(new Attribute("propietario", "1"));
+        Element datos_cuenta = new Element("dato_cuenta");
         datos_cuenta.addContent(new Element("nombre").setText("yong"));
         datos_cuenta.addContent(new Element("numerocuenta").setText("123456"));
         doc.getRootElement().addContent(datos_cuenta);
@@ -34,7 +33,7 @@ public class xmlcuenta {
 
         doc.getRootElement().addContent(infocuenta);
 
-         Element datos_cuenta2 = new Element("cuentaTickets");
+         Element datos_cuenta2 = new Element("datos_cuenta");
         datos_cuenta2.addContent(new Element("nombreTickets").setText("Metallica"));
         datos_cuenta2.addContent(new Element("cuentaDeposito").setText("800"));
 
